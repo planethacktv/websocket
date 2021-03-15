666b9af4dfbe7458a162e98c988ee1fe563e573a
Randy Apuzzo <apuzzo@gmail.com>
2021-03-14 21:34:30 -0700

    updated websocket handle multiple players

--- index.js
+++ index.js
@@ -2,32 +2,34 @@ const WebSocket = require('ws');
 
 const port = process.env.PORT || 8080;
 
-sprites = [
-  {
+sprites = {
+  'monster1' : {
     'uid' : '12345',
     'name' : 'monster',
     'top' : '100',
     'left' : '200'
   }
-]
+}
 
 const wss = new WebSocket.Server({ port: port });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, request, client) {
+  
   ws.on('message', function incoming(data) {
+    
+    console.log(`Received message ${data} from user ${client}`);
+    
     wss.clients.forEach(function each(client) {
-     
-        //client.send(data);
-        console.log(`received:`);
-        console.log(data);
+     // if (client !== ws && client.readyState === WebSocket.OPEN) {
+  
         let tempPlayer = JSON.parse(data);
-        sprites.pop(tempPlayer)
+        sprites[tempPlayer.uid] = tempPlayer
         console.log(sprites)
         // add player data to sprites array
         // check if the sprites array already has unique
        
         client.send(JSON.stringify(sprites));
-      
+      //}
     });
   });
 });
